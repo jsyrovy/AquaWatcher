096c9abd8bddc3086297ba1f694c31c0ce91a950
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2023-07-15 10:09:23 +0200

    Fix #1 wellness

--- downloader.py
+++ downloader.py
@@ -19,15 +19,13 @@ def get_record() -> Record:
     soup = bs4.BeautifulSoup(r.content, "html.parser")
 
     values = soup.find("div", class_="fast-info").find_all("span")
+    assert len(values) == 4, f"Unexpected number of values: {len(values)}"
 
-    if len(values) == 2:
-        record = Record(pool=int(values[0].text), aqua=0, wellness=int(values[1].text))
-    else:
-        record = Record(
-            pool=int(values[0].text),
-            aqua=int(values[1].text),
-            wellness=int(values[2].text),
-        )
+    record = Record(
+        pool=int(values[0].text),
+        aqua=int(values[1].text),
+        wellness=int(values[3].text),
+    )
 
     print(f"Data were downloaded: {record}")
 
