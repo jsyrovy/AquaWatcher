334df0b27e62893c14fc5bf8453d678cffeaac4b
Jiri Syrovy <jiri.syrovy@firma.seznam.cz>
2023-09-11 08:37:46 +0200

    Fix parsing

--- downloader.py
+++ downloader.py
@@ -19,12 +19,12 @@ def get_record() -> Record:
     soup = bs4.BeautifulSoup(r.content, "html.parser")
 
     values = soup.find("div", class_="fast-info").find_all("span")
-    assert len(values) == 4, f"Unexpected number of values: {len(values)}"
+    assert len(values) == 3, f"Unexpected number of values: {len(values)}"
 
     record = Record(
         pool=int(values[0].text),
         aqua=int(values[1].text),
-        wellness=int(values[3].text),
+        wellness=int(values[2].text),
     )
 
     print(f"Data were downloaded: {record}")
